5e362b849db56081d5e027e7301a0b184142423b
baba_s <shotababa1206@gmail.com>
2020-05-01 20:10:02 +0900

    first commit

---
+++ Editor/ConstStringCodeGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+using System.Text;
+
+namespace UniConstStringCodeGenerator
+{
+	/// <summary>
+	/// 文字列の定数のコードを生成するクラス
+	/// </summary>
+	public static class ConstStringCodeGenerator
+	{
+		//================================================================================
+		// 定数
+		//================================================================================
+		public const string TAG_NAMESPACE_NAME      = "#NAMESPACE_NAME#";
+		public const string TAG_CLASS_NAME          = "#CLASS_NAME#";
+		public const string TAG_CLASS_COMMENT       = "#CLASS_COMMENT#";
+		public const string TAG_VALUES              = "#VALUES#";
+		public const string TAG_LENGTH              = "#LENGTH#";
+		public const string TAG_GET_VALUES_CONTENTS = "#GET_VALUES_CONTENTS#";
+
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// 指定されたパスに文字列の定数のコードを書き込みます
+		/// </summary>
+		public static void Write( string path, ConstStringCodeGeneratorOptions options )
+		{
+			var code = Generate( options );
+			Write( path, code );
+		}
+
+		/// <summary>
+		/// 指定されたパスに指定された文字列を書き込みます
+		/// </summary>
+		public static void Write( string path, string code )
+		{
+			var dir = Path.GetDirectoryName( path );
+
+			if ( string.IsNullOrWhiteSpace( dir ) ) return;
+
+			if ( !Directory.Exists( dir ) )
+			{
+				Directory.CreateDirectory( dir );
+			}
+
+			File.WriteAllText( path, code );
+		}
+
+		/// <summary>
+		/// 文字列の定数のコードを表現する文字列を生成して返します
+		/// </summary>
+		public static string Generate( ConstStringCodeGeneratorOptions options )
+		{
+			var template          = options.Template;
+			var namespaceName     = options.NamespaceName;
+			var className         = options.ClassName;
+			var classComment      = options.ClassComment;
+			var length            = options.Elements.Length;
+			var values            = GetValuesText( options );
+			var getValuesContents = GetGetValuesContents( options );
+			var output            = template;
+
+			output = output.Replace( TAG_NAMESPACE_NAME, namespaceName );
+			output = output.Replace( TAG_CLASS_NAME, className );
+			output = output.Replace( TAG_CLASS_COMMENT, classComment );
+			output = output.Replace( TAG_VALUES, values );
+			output = output.Replace( TAG_LENGTH, length.ToString() );
+			output = output.Replace( TAG_GET_VALUES_CONTENTS, getValuesContents );
+
+			return output;
+		}
+
+		/// <summary>
+		/// 文字列の定数の要素を定義するコードを生成して返します
+		/// </summary>
+		private static string GetValuesText( ConstStringCodeGeneratorOptions options )
+		{
+			var builder = new StringBuilder();
+
+			foreach ( var element in options.Elements )
+			{
+				var comment = element.Comment;
+				var name    = element.Name;
+				var value   = element.Value;
+
+				// コメントが複数行の場合も考慮
+				builder.Append( "\t\t" ).AppendLine( "///<summary>" );
+				foreach ( var n in comment.Split( '\n' ) )
+				{
+					builder.Append( "\t\t" ).AppendFormat( "///<para>{0}</para>", n ).AppendLine();
+				}
+
+				builder.Append( "\t\t" ).AppendLine( "///</summary>" );
+				builder.Append( "\t\t" ).AppendFormat( @"public const string {0} = ""{1}"";", name, value ).AppendLine();
+			}
+
+			return builder.ToString().TrimEnd();
+		}
+
+		/// <summary>
+		/// 文字列の定数の一覧を返す関数の中身のコードを生成して返します
+		/// </summary>
+		private static string GetGetValuesContents( ConstStringCodeGeneratorOptions options )
+		{
+			var builder = new StringBuilder();
+
+			foreach ( var element in options.Elements )
+			{
+				var name = element.Name;
+
+				builder.Append( "\t\t\t" ).AppendFormat( @"yield return {0};", name ).AppendLine();
+			}
+
+			return builder.ToString().TrimEnd();
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,130 @@
+# Uni Const String Code Generator
+
+* 文字列の定数のコードを生成するエディタ拡張
+
+## 使用例
+
+### JobType 型のコードを生成するエディタ拡張
+
+```cs
+using UniConstStringCodeGenerator;
+using UnityEditor;
+
+public class Example
+{
+    [MenuItem( "Tools/Generate" )]
+    private static void Generate()
+    {
+        var template = @"using System.Collections.Generic;
+
+namespace #NAMESPACE_NAME#
+{
+    /// <summary>
+    /// #CLASS_COMMENT#
+    /// </summary>
+    public static partial class #CLASS_NAME#
+    {
+        /// <summary>
+        /// 要素数
+        /// </summary>
+        public const int LENGTH = #LENGTH#;
+
+#VALUES#
+        
+        /// <summary>
+        /// 文字列の定数の一覧を返します
+        /// </summary>
+        public static IEnumerable<string> GetValues()
+        {
+#GET_VALUES_CONTENTS#
+        }
+    }
+}
+";
+        var values = new[]
+        {
+            new ConstStringCodeGeneratorOptions.Element { Name = "NONE", Comment      = "無効", Value   = "NONE" },
+            new ConstStringCodeGeneratorOptions.Element { Name = "SOLDIER", Comment   = "王国兵士", Value = "SOLDIER" },
+            new ConstStringCodeGeneratorOptions.Element { Name = "SORCERER", Comment  = "魔法使い", Value = "SORCERER" },
+            new ConstStringCodeGeneratorOptions.Element { Name = "HUNTER", Comment    = "狩人", Value   = "HUNTER" },
+            new ConstStringCodeGeneratorOptions.Element { Name = "MERCENARY", Comment = "傭兵", Value   = "MERCENARY" },
+        };
+
+        var options = new ConstStringCodeGeneratorOptions
+        {
+            Template      = template,
+            NamespaceName = "MyProject",
+            ClassName     = "JobType",
+            ClassComment  = "ジョブの種類",
+            Elements      = values,
+        };
+
+        var path = "Assets/JobType.cs";
+        var code = ConstStringCodeGenerator.Generate( options );
+
+        code = code
+                .Replace( "\t", "    " )
+                .Replace( "\r\n", "#NEW_LINE#" )
+                .Replace( "\r", "#NEW_LINE#" )
+                .Replace( "\n", "#NEW_LINE#" )
+                .Replace( "#NEW_LINE#", "\r\n" )
+            ;
+
+        ConstStringCodeGenerator.Write( path, code );
+        AssetDatabase.Refresh();
+    }
+}
+```
+
+### 生成された JobType 型のコード
+
+```cs
+using System.Collections.Generic;
+
+namespace MyProject
+{
+    /// <summary>
+    /// ジョブの種類
+    /// </summary>
+    public static partial class JobType
+    {
+        /// <summary>
+        /// 要素数
+        /// </summary>
+        public const int LENGTH = 5;
+
+        ///<summary>
+        ///<para>無効</para>
+        ///</summary>
+        public const string NONE = "NONE";
+        ///<summary>
+        ///<para>王国兵士</para>
+        ///</summary>
+        public const string SOLDIER = "SOLDIER";
+        ///<summary>
+        ///<para>魔法使い</para>
+        ///</summary>
+        public const string SORCERER = "SORCERER";
+        ///<summary>
+        ///<para>狩人</para>
+        ///</summary>
+        public const string HUNTER = "HUNTER";
+        ///<summary>
+        ///<para>傭兵</para>
+        ///</summary>
+        public const string MERCENARY = "MERCENARY";
+        
+        /// <summary>
+        /// 文字列の定数の一覧を返します
+        /// </summary>
+        public static IEnumerable<string> GetValues()
+        {
+            yield return NONE;
+            yield return SOLDIER;
+            yield return SORCERER;
+            yield return HUNTER;
+            yield return MERCENARY;
+        }
+    }
+}
+```
